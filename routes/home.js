1d0f398fbf23d942e88bfb02c025e75ffcb3700d
MuGuo <874005743@qq.com>
2015-08-03 22:45:35 +0800

    8.3 22:30

--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,16 @@
 var express = require('express');
 var router = express.Router();
 
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'qilu'
+});
+
+
 router.get('/', function(req, res, next) {
     res.render('home', { title: 'Express' });
 });
@@ -12,15 +22,29 @@ router.get('/', function(req, res, next) {
 router.post('/land', function(req, res) {
     console.log(req.body.name);
     console.log(req.body.password);
-    if (req.body.name == 'amy') {
-        res.json({
-            message: 'success'
-        });
-    } else {
-        res.json({
-            message: 'failure'
-        })
-    }
+
+    connection.query('SELECT * from user', function(err, rows) {
+        // connected! (unless `err` is set)
+        if (rows.length > 0) {
+            res.json({
+                message: 'success'
+            });
+        } else {
+            res.json({
+                message: 'failure'
+            })
+        }
+    });
+    //
+    //if (req.body.name == 'amy') {
+    //    res.json({
+    //        message: 'success'
+    //    });
+    //} else {
+    //    res.json({
+    //        message: 'failure'
+    //    })
+    //}
 });
 
 router.post('/register', function(req, res) {
