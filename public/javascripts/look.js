0a3f66e73e8b8be9ad04df9fcf331e02f63cb67e
sealday <sealday@gmail.com>
2015-09-20 16:04:31 +0800

    code: remove unused codes

--- public/javascripts/look.js
+++ public/javascripts/look.js
@@ -13,6 +13,7 @@
     ]);
     //一个定时器，监视弹幕时间并更新到页面上
     starter();
+
     function starter(){
         danmu.danmu('danmuStart');
     }
@@ -39,26 +40,15 @@
         var position = document.getElementById('position').value;
         var time = $('#danmu').data("nowTime")+1;
         var size =document.getElementById('text_size').value;
-        //var text_obj='{ "text":"'+text+'","color":"'+color+'","size":"'+size+'","position":"'+position+'","time":'+time+'}';
-        //$.post("stone.php",{danmu:text_obj});
-//        var text_obj='{ "text":"'+text+'","color":"'+color+'","size":"'+size+'","position":"'+position+'","time":'+time+',"isnew":""}';
+
         var text_obj = {
             text: text,
             color: color,
             size: size,
             position: position,
-            time: danmu.data("nowTime") + 1
+            time: time
         };
 
-//        text_obj = { text:"这是底部弹幕" , color:"red" ,size:1,position:2,time:2};
-//        var new_obj=eval('('+text_obj+')');
-
-
-//
-
-
-
-        //stoper();
         danmu.danmu("addDanmu", text_obj);
         resumer();
         document.getElementById('text').value='';
@@ -80,7 +70,6 @@
 
         }
     }
-
     //设置弹幕时间
     function settime(){
         var t=document.getElementById("set_time").value;
@@ -88,77 +77,7 @@
         $('#danmu').danmu("setTime",t);
         starter();
     }
-//    var danmu = $('#danmu');
-//    function pauser(){
-//        danmu.danmu('danmuPause');
-//    }
-//    function resumer(){
-//        danmu.danmu('danmuResume');
-//    }
-//    function stoper(){
-//        danmu.danmu('danmuStop');
-//    }
-//    function getime(){
-//        alert(danmu.data("nowTime"));
-//    }
-//    function getpaused(){
-//        alert(danmu.data("paused"));
-//    }
-//
-////发送弹幕，使用了文档README.md第7节中推荐的方法
-//    function send(){
-//        var text = document.getElementById('text').value;
-//        var color = document.getElementById('color').value;
-//        var position = document.getElementById('position').value;
-//        var time = danmu.data("nowTime")+1;
-//        var size =document.getElementById('text_size').value;
-//        //var text_obj='{ "text":"'+text+'","color":"'+color+'","size":"'+size+'","position":"'+position+'","time":'+time+'}';
-//        //$.post("stone.php",{danmu:text_obj});
-//        var text_obj='{ "text":"'+text+'","color":"'+color+'","size":"'+size+'","position":"'+position+'","time":'+time+',"isnew":""}';
-//        var new_obj=eval('('+text_obj+')');
-//        danmu.danmu("addAanmu", new_obj);
-//        document.getElementById('text').value='';
-//    }
-////调整透明度函数
-//    function op(){
-//        var op=document.getElementById('op').value;
-//        danmu.danmu("setOpacity",op/100);
-//    }
-//
-////调隐藏 显示
-//    function changehide() {
-//        var op = document.getElementById('op').value;
-//        op = op / 100;
-//        if (document.getElementById("ishide").checked) {
-//            $("#danmu").danmu("setOpacity",1)
-//        } else {
-//            $("#danmu").danmu("setOpacity",0)
-//
-//        }
-//    }
-//    danmu.danmu({});
-//    danmu.danmu('addDanmu', {
-//        text: '底部',
-//        color: 'red',
-//        size: 1,
-//        position: 2,
-//        time: 2
-//    });
-////设置弹幕时间
-//    function settime(){
-//        var t=document.getElementById("set_time").value;
-//        t=parseInt(t)
-//        danmu.danmu("setTime",t);
-//    }
-//
-//    $("#send").click(function(){
-//alert('--');
-//        danmu.danmu('addDanmu', {
-//            text: '底部',
-//            color: 'red',
-//            size: 1,
-//            position: 2,
-//            time: 2
-//        })
-//    });
+
+
+    // 评论部分
 })();
